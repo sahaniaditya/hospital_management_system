f7a7b48baf8bfdb9e252784305f8c3b3fc942bc5
akshatb22me007 <120656132+akshatb22me007@users.noreply.github.com>
2023-05-22 15:17:01 +0530

    Update home.c

--- home.c
+++ home.c
@@ -1,24 +1,41 @@
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <openssl/md5.h>
+#include <termios.h>
+#include <unistd.h>
 
+FILE *fptr;
 char hospital_name[] = "IITJ HOSPIAL";
 char password[100];
 int opt;
+
+
 void baseline();
 void generateMD5Hash(const char *password, unsigned char *hash);
 void doctor_nextmenu();
 void doctor_menu();
 void patient_menu();
-
-
+void disableEcho();
+void enableEcho();
+int createPatient(FILE*);
+int patientId(int,int);
+void createDate(char*);
+struct Patient{
+    char name[100];
+    int age;
+    int dob;
+    char sex[100];
+    long contact;
+    char email[100];
+    char address[100];
+};
 int main(){
 
-    FILE *fptr;
-    fptr = fopen("Patients/aditya.txt","w");
-    fprintf(fptr,"Hello");
-    fclose(fptr);
+
     for(int i=0;i<10;i++) printf(" ");
     for(int i=0 ; i< 63 ; i++)
     {
@@ -46,15 +63,68 @@ int main(){
     	break;
     	default : printf("\nINVALID OPTION\n");
     }
+
     
     return 0;
 }
+int createPatient(FILE *fptr){
+
+    struct Patient patient1;
+    //Code to input patient details
+    printf("Enter your full name : ");
+    getchar();
+    fgets(patient1.name,100,stdin);
+    printf("Enter your sex : ");
+    fgets(patient1.sex,100,stdin);
+    printf("Enter your email : ");
+    fgets(patient1.email,100,stdin);
+    printf("Enter your address : ");
+    fgets(patient1.address,100,stdin);
+    printf("Enter your DOB(DDMMYYYY) : ");
+    scanf("%d",&patient1.dob);
+    getchar();
+    printf("Enter your age : ");
+    scanf("%d",&patient1.age);
+    printf("Enter your phone number : ");
+    scanf("%ld",&patient1.contact);
+    //Code to generate Patient Id
+    int patientid = patientId(patient1.age,patient1.dob);
+
+    //to get the date of record creation
+    char date[100];
+    createDate(date); 
+
+    //Code to create patient file
+    fptr = fopen("Patients/patient1.txt","w");
+    fprintf(fptr,"PatientID : %d                      Date : %s",patientid,date);
+    fprintf(fptr,"Name : %s",patient1.name);
+    fprintf(fptr,"Age : %d\n",patient1.age);
+    fprintf(fptr,"Sex : %s",patient1.sex);
+    fprintf(fptr,"Phone : %ld\n",patient1.contact);
+    fprintf(fptr,"Email : %s",patient1.email);
+    fprintf(fptr,"Address : %s",patient1.address);
+    fptr = fopen("Patients/aditya.txt","w");
+    fprintf(fptr,"Hello");
+    fclose(fptr);
+ 
+    return 0;
+}
+
 
 
 
 void baseline() {
 for(int i=0 ; i< 166 ; i++) printf("*");
 }
+int patientId(int age,int dob){
+    int id = 0;
+   while(age != 0){
+        id = id * 10 + (age % 10);
+        age = age / 10;
+   }
+   id = id + dob;
+   return id;
+   }
 void generateMD5Hash(const char *password, unsigned char *hash) {
     MD5_CTX md5Context;
     MD5_Init(&md5Context);
@@ -65,11 +135,13 @@ void doctor_nextmenu() {
 	baseline();
 	printf("1.Patient Record(Press 0)\n2.Scheduled vist(Press 1)\nEnter any option : \n");
 	scanf("%d",&opt);
-	
+
 }
 void doctor_menu() {
     printf("\nEnter password : \n");
+    disableEcho();
     scanf("%s",password);
+    enableEcho();
     printf("\n");
     unsigned char hash[MD5_DIGEST_LENGTH];
     generateMD5Hash(password, hash);
@@ -88,5 +160,31 @@ void doctor_menu() {
 void patient_menu() {
 	printf("\n1. NEW \n2. EXISTING\nEnter any option :");
 	scanf("%d",&opt);
-		
+	switch(opt)
+	{
+		case 1: createPatient(fptr);
+	}
+
+}
+void createDate(char *date ){
+struct tm* ptr;
+    time_t t;
+    t = time(NULL);
+    ptr = localtime(&t);
+    char d[100];
+    strcpy(date,asctime(ptr));
+
+}
+void disableEcho() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag &= ~ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
+}
+
+void enableEcho() {
+    struct termios term;
+    tcgetattr(STDIN_FILENO, &term);
+    term.c_lflag |= ECHO;
+    tcsetattr(STDIN_FILENO, TCSANOW, &term);
 }
